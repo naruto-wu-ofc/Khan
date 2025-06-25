eb5aa3e191ff43a224d57faba2fbb45453e2832b
naruto-wu-ofc <narutosublk@gmail.com>
2025-06-25 08:34:46 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "I3tnxIxb#La_zC4-s9s3m3ONF-70xpmb7aXk5OoaB_X-a6xliG7c",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
